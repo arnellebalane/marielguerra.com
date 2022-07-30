222c4e523f22fc50840303dd6f74346114944efa
Arnelle Balane <arnellebalane@gmail.com>
2022-07-30 23:27:20 +0800

    Replace favicon and add social meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,19 @@ function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.svg" />
+        <link rel="icon" href="/favicon.png" />
+        <title>Mariel Guerra</title>
+
+        <meta property="og:title" content="Mariel Guerra" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content={`${process.env.SITE_URL}/thumbnail.jpg`}
+        />
+        <meta property="og:url" content={`${process.env.SITE_URL}`} />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
+
       <Component {...pageProps} />
     </>
   );
