bb0155f002665b94bd984550e7ab4b3f584f09cc
Arnelle Balane <arnellebalane@gmail.com>
2022-08-10 21:03:32 +0800

    Add progress bar during client-side navigation

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,26 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import nprogress from 'nprogress';
+import 'nprogress/nprogress.css';
 import '../styles/index.css';
 
 function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    nprogress.configure({ showSpinner: false });
+    const handleRouteChangeStart = () => nprogress.start();
+    const handleRouteChangeComplete = () => nprogress.done();
+
+    router.events.on('routeChangeStart', handleRouteChangeStart);
+    router.events.on('routeChangeComplete', handleRouteChangeComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart);
+      router.events.off('routeChangeComplete', handleRouteChangeComplete);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
