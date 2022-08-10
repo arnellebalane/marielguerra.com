3e75867a4dc434b9c3e4f4c84d1dd615b41dda80
Arnelle Balane <arnellebalane@gmail.com>
2022-08-10 21:03:14 +0800

    Use constructable stylesheets to set global theme style

--- components/layouts/ThemedLayout.jsx
+++ components/layouts/ThemedLayout.jsx
@@ -1,16 +1,34 @@
-import * as S from './ThemedLayout.styles';
+import { useEffect, useState } from 'react';
 
 const ThemedLayout = ({ children, themeStyle }) => {
-  const theme = {
-    '--bg-body': themeStyle['bg-body'],
-    '--primary': themeStyle['primary'],
-    '--fg-primary': themeStyle['fg-primary'],
-    '--inverted': themeStyle['inverted'],
-    '--fg-inverted': themeStyle['fg-inverted'],
-    '--complementary': themeStyle['complementary'],
-    '--fg-complementary': themeStyle['fg-complementary'],
-  };
-  return <S.Layout style={theme}>{children}</S.Layout>;
+  const [sheet, setSheet] = useState(null);
+
+  useEffect(() => {
+    if (sheet) {
+      document.adoptedStyleSheets = [sheet];
+    } else {
+      setSheet(new CSSStyleSheet());
+    }
+  }, [sheet]);
+
+  useEffect(() => {
+    if (!sheet) {
+      return;
+    }
+    sheet.replace(`
+      :root {
+        --bg-body: ${themeStyle['bg-body']};
+        --primary: ${themeStyle['primary']};
+        --fg-primary: ${themeStyle['fg-primary']};
+        --inverted: ${themeStyle['inverted']};
+        --fg-inverted: ${themeStyle['fg-inverted']};
+        --complementary: ${themeStyle['complementary']};
+        --fg-complementary: ${themeStyle['fg-complementary']};
+      }
+    `);
+  }, [sheet, themeStyle]);
+
+  return children;
 };
 
 export default ThemedLayout;
